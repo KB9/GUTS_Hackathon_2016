ff9c13f30bac41cac13a75e68f1b3a9b8529e441
Bruno Manganelli <bruno.manga95@gmail.com>
2016-10-16 03:12:11 +0100

    Oh god

--- aRSimpleNativeCars/src/main/obj/local/armeabi/objs/ARWrapperNativeCarsExample/ARWrapperNativeCarsExample.o.d
+++ aRSimpleNativeCars/src/main/obj/local/armeabi/objs/ARWrapperNativeCarsExample/ARWrapperNativeCarsExample.o.d
@@ -20,13 +20,72 @@
   jni/../../../../../../android/../include\ARWrapper/Platform.h \
   jni/../../../../../../android/../include\ARWrapper/Image.h \
   jni/../../../../../../android/../include\ARWrapper/Error.h jni/Car.h \
+  jni/CarControls.h jni/World.h jni/glm/glm.hpp \
+  jni/glm/detail/_fixes.hpp \
+  C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cmath \
+  C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\climits \
+  C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cfloat \
+  C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\float.h \
+  C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\limits \
+  C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cassert \
+  jni/glm/fwd.hpp jni/glm/detail/type_int.hpp jni/glm/detail/setup.hpp \
+  jni/glm/detail/../simd/platform.h \
+  C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstdint \
+  jni/glm/detail/type_float.hpp jni/glm/detail/type_vec.hpp \
+  jni/glm/detail/precision.hpp jni/glm/detail/type_mat.hpp \
+  jni/glm/vec2.hpp jni/glm/detail/type_vec2.hpp \
+  jni/glm/detail/type_vec2.inl jni/glm/vec3.hpp \
+  jni/glm/detail/type_vec3.hpp jni/glm/detail/type_vec3.inl \
+  jni/glm/vec4.hpp jni/glm/detail/type_vec4.hpp \
+  jni/glm/detail/type_vec4.inl jni/glm/detail/type_vec4_simd.inl \
+  jni/glm/mat2x2.hpp jni/glm/detail/type_mat2x2.hpp \
+  jni/glm/detail/type_mat2x2.inl jni/glm/detail/func_matrix.hpp \
+  jni/glm/detail/../mat2x3.hpp jni/glm/detail/type_mat2x3.hpp \
+  jni/glm/detail/type_mat2x3.inl jni/glm/detail/../mat2x4.hpp \
+  jni/glm/detail/type_mat2x4.hpp jni/glm/detail/type_mat2x4.inl \
+  jni/glm/detail/../mat3x2.hpp jni/glm/detail/type_mat3x2.hpp \
+  jni/glm/detail/type_mat3x2.inl jni/glm/detail/../mat3x3.hpp \
+  jni/glm/detail/type_mat3x3.hpp jni/glm/detail/type_mat3x3.inl \
+  jni/glm/detail/../mat3x4.hpp jni/glm/detail/type_mat3x4.hpp \
+  jni/glm/detail/type_mat3x4.inl jni/glm/detail/../mat4x2.hpp \
+  jni/glm/detail/type_mat4x2.hpp jni/glm/detail/type_mat4x2.inl \
+  jni/glm/detail/../mat4x3.hpp jni/glm/detail/type_mat4x3.hpp \
+  jni/glm/detail/type_mat4x3.inl jni/glm/detail/../mat4x4.hpp \
+  jni/glm/detail/type_mat4x4.hpp jni/glm/detail/type_mat4x4.inl \
+  jni/glm/detail/type_mat4x4_simd.inl jni/glm/detail/func_matrix.inl \
+  jni/glm/detail/../geometric.hpp jni/glm/detail/func_geometric.hpp \
+  jni/glm/detail/func_geometric.inl jni/glm/detail/func_exponential.hpp \
+  jni/glm/detail/type_vec1.hpp jni/glm/detail/type_vec1.inl \
+  jni/glm/detail/func_exponential.inl \
+  jni/glm/detail/func_vector_relational.hpp \
+  jni/glm/detail/func_vector_relational.inl \
+  jni/glm/detail/func_vector_relational_simd.inl \
+  jni/glm/detail/_vectorize.hpp jni/glm/detail/func_exponential_simd.inl \
+  jni/glm/detail/../simd/exponential.h jni/glm/detail/func_common.hpp \
+  jni/glm/detail/func_common.inl jni/glm/detail/func_common_simd.inl \
+  jni/glm/detail/func_geometric_simd.inl \
+  jni/glm/detail/../simd/geometric.h jni/glm/detail/../simd/common.h \
+  jni/glm/detail/func_matrix_simd.inl jni/glm/trigonometric.hpp \
+  jni/glm/detail/func_trigonometric.hpp \
+  jni/glm/detail/func_trigonometric.inl \
+  jni/glm/detail/func_trigonometric_simd.inl jni/glm/exponential.hpp \
+  jni/glm/common.hpp jni/glm/packing.hpp jni/glm/detail/func_packing.hpp \
+  jni/glm/detail/func_packing.inl jni/glm/detail/type_half.hpp \
+  jni/glm/detail/type_half.inl jni/glm/detail/func_packing_simd.inl \
+  jni/glm/matrix.hpp jni/glm/vector_relational.hpp jni/glm/integer.hpp \
+  jni/glm/detail/func_integer.hpp jni/glm/detail/func_integer.inl \
+  jni/glm/detail/func_integer_simd.inl jni/glm/detail/../simd/integer.h \
+  jni/glm/gtc/type_ptr.hpp jni/glm/gtc/../gtc/quaternion.hpp \
+  jni/glm/gtc/../gtc/constants.hpp jni/glm/gtc/constants.inl \
+  jni/glm/gtc/quaternion.inl jni/glm/gtc/quaternion_simd.inl \
+  C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstring \
+  jni/glm/gtc/type_ptr.inl jni/Landmarks.h \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\vector \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\iosfwd \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\wchar.h \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__bit_reference \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\algorithm \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\initializer_list \
-  C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstring \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\utility \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__tuple \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\memory \
@@ -34,19 +93,15 @@
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\exception \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstdio \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstdlib \
-  C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstdint \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\new \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__undef___deallocate \
-  C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\limits \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\iterator \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__functional_base \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__debug \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\tuple \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\stdexcept \
-  C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cassert \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\atomic \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__undef_min_max \
-  C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\climits \
   C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__split_buffer
 
 jni/../../../../../../android/../include\AR/gsub_es.h:
@@ -91,6 +146,220 @@ jni/../../../../../../android/../include\ARWrapper/Error.h:
 
 jni/Car.h:
 
+jni/CarControls.h:
+
+jni/World.h:
+
+jni/glm/glm.hpp:
+
+jni/glm/detail/_fixes.hpp:
+
+C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cmath:
+
+C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\climits:
+
+C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cfloat:
+
+C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\float.h:
+
+C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\limits:
+
+C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cassert:
+
+jni/glm/fwd.hpp:
+
+jni/glm/detail/type_int.hpp:
+
+jni/glm/detail/setup.hpp:
+
+jni/glm/detail/../simd/platform.h:
+
+C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstdint:
+
+jni/glm/detail/type_float.hpp:
+
+jni/glm/detail/type_vec.hpp:
+
+jni/glm/detail/precision.hpp:
+
+jni/glm/detail/type_mat.hpp:
+
+jni/glm/vec2.hpp:
+
+jni/glm/detail/type_vec2.hpp:
+
+jni/glm/detail/type_vec2.inl:
+
+jni/glm/vec3.hpp:
+
+jni/glm/detail/type_vec3.hpp:
+
+jni/glm/detail/type_vec3.inl:
+
+jni/glm/vec4.hpp:
+
+jni/glm/detail/type_vec4.hpp:
+
+jni/glm/detail/type_vec4.inl:
+
+jni/glm/detail/type_vec4_simd.inl:
+
+jni/glm/mat2x2.hpp:
+
+jni/glm/detail/type_mat2x2.hpp:
+
+jni/glm/detail/type_mat2x2.inl:
+
+jni/glm/detail/func_matrix.hpp:
+
+jni/glm/detail/../mat2x3.hpp:
+
+jni/glm/detail/type_mat2x3.hpp:
+
+jni/glm/detail/type_mat2x3.inl:
+
+jni/glm/detail/../mat2x4.hpp:
+
+jni/glm/detail/type_mat2x4.hpp:
+
+jni/glm/detail/type_mat2x4.inl:
+
+jni/glm/detail/../mat3x2.hpp:
+
+jni/glm/detail/type_mat3x2.hpp:
+
+jni/glm/detail/type_mat3x2.inl:
+
+jni/glm/detail/../mat3x3.hpp:
+
+jni/glm/detail/type_mat3x3.hpp:
+
+jni/glm/detail/type_mat3x3.inl:
+
+jni/glm/detail/../mat3x4.hpp:
+
+jni/glm/detail/type_mat3x4.hpp:
+
+jni/glm/detail/type_mat3x4.inl:
+
+jni/glm/detail/../mat4x2.hpp:
+
+jni/glm/detail/type_mat4x2.hpp:
+
+jni/glm/detail/type_mat4x2.inl:
+
+jni/glm/detail/../mat4x3.hpp:
+
+jni/glm/detail/type_mat4x3.hpp:
+
+jni/glm/detail/type_mat4x3.inl:
+
+jni/glm/detail/../mat4x4.hpp:
+
+jni/glm/detail/type_mat4x4.hpp:
+
+jni/glm/detail/type_mat4x4.inl:
+
+jni/glm/detail/type_mat4x4_simd.inl:
+
+jni/glm/detail/func_matrix.inl:
+
+jni/glm/detail/../geometric.hpp:
+
+jni/glm/detail/func_geometric.hpp:
+
+jni/glm/detail/func_geometric.inl:
+
+jni/glm/detail/func_exponential.hpp:
+
+jni/glm/detail/type_vec1.hpp:
+
+jni/glm/detail/type_vec1.inl:
+
+jni/glm/detail/func_exponential.inl:
+
+jni/glm/detail/func_vector_relational.hpp:
+
+jni/glm/detail/func_vector_relational.inl:
+
+jni/glm/detail/func_vector_relational_simd.inl:
+
+jni/glm/detail/_vectorize.hpp:
+
+jni/glm/detail/func_exponential_simd.inl:
+
+jni/glm/detail/../simd/exponential.h:
+
+jni/glm/detail/func_common.hpp:
+
+jni/glm/detail/func_common.inl:
+
+jni/glm/detail/func_common_simd.inl:
+
+jni/glm/detail/func_geometric_simd.inl:
+
+jni/glm/detail/../simd/geometric.h:
+
+jni/glm/detail/../simd/common.h:
+
+jni/glm/detail/func_matrix_simd.inl:
+
+jni/glm/trigonometric.hpp:
+
+jni/glm/detail/func_trigonometric.hpp:
+
+jni/glm/detail/func_trigonometric.inl:
+
+jni/glm/detail/func_trigonometric_simd.inl:
+
+jni/glm/exponential.hpp:
+
+jni/glm/common.hpp:
+
+jni/glm/packing.hpp:
+
+jni/glm/detail/func_packing.hpp:
+
+jni/glm/detail/func_packing.inl:
+
+jni/glm/detail/type_half.hpp:
+
+jni/glm/detail/type_half.inl:
+
+jni/glm/detail/func_packing_simd.inl:
+
+jni/glm/matrix.hpp:
+
+jni/glm/vector_relational.hpp:
+
+jni/glm/integer.hpp:
+
+jni/glm/detail/func_integer.hpp:
+
+jni/glm/detail/func_integer.inl:
+
+jni/glm/detail/func_integer_simd.inl:
+
+jni/glm/detail/../simd/integer.h:
+
+jni/glm/gtc/type_ptr.hpp:
+
+jni/glm/gtc/../gtc/quaternion.hpp:
+
+jni/glm/gtc/../gtc/constants.hpp:
+
+jni/glm/gtc/constants.inl:
+
+jni/glm/gtc/quaternion.inl:
+
+jni/glm/gtc/quaternion_simd.inl:
+
+C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstring:
+
+jni/glm/gtc/type_ptr.inl:
+
+jni/Landmarks.h:
+
 C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\vector:
 
 C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\iosfwd:
@@ -102,8 +371,6 @@ C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-st
 C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\algorithm:
 
 C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\initializer_list:
-
-C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstring:
 
 C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\utility:
 
@@ -119,13 +386,9 @@ C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-st
 
 C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstdlib:
 
-C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstdint:
-
 C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\new:
 
 C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__undef___deallocate:
-
-C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\limits:
 
 C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\iterator:
 
@@ -137,12 +400,8 @@ C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-st
 
 C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\stdexcept:
 
-C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cassert:
-
 C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\atomic:
 
 C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__undef_min_max:
-
-C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\climits:
 
 C:/Users/bruno_000/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__split_buffer:
